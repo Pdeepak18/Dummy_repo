fcab5873a84301ccce28f5516319548bfc0aae6f
Pdeepak18 <85224426+Pdeepak18@users.noreply.github.com>
2023-01-08 19:03:05 +0530

    subtract function

--- sum.cpp
+++ sum.cpp
@@ -1,8 +1,14 @@
 #include<iostream>
 using namespace std;
 
+
+int sub(int a,int b){
+   return b>a ?  (b-a) :  (a-b) ;
+
+}
 int main(){
     int a,b;
     cin>>a>>b;
+    cout<<sub(a,b)<<endl;
     cout<<a+b;
 }
